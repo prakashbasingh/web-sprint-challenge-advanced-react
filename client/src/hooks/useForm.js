0034a90039556b45eb599414c44176088108b87f
unknown <prakashbahadursingh2@gmail.com>
2020-05-08 09:50:33 -0700

    succefully fetched data and able to display plants on the browser

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,14 @@
 // write your custom hook here to control your checkout form
+// export const useForm = (key, initialValue) => {
+
+//     const[values, setValues] = useLocalStorage(key, initialValue);
+
+//     const handleChanges = e => {
+//         setValues({
+//             ...values,
+//             [e.target.name]: e.target.value
+//         });
+//     };
+
+//     return [value, handleChanges]
+// }
