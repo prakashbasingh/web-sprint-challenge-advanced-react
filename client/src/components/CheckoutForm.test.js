9ae5331d21ad23050d1b0c34c7f33ff21f69a721
unknown <prakashbahadursingh2@gmail.com>
2020-05-08 10:54:17 -0700

    first test confirmed text checkout-form is visible and in document

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+
 import CheckoutForm from "./CheckoutForm";
+
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+ const {getByText} = render(<CheckoutForm/>);
+ const header = getByText(/checkout form/i);
+ expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+const button = getByText(/checkout/i);
+    // fireEvent.click(button);
+
+
+});
